1a9200ad27fcca5d98945cc08a55128c13372e79
VICTORIAMK3\Jude <reien1012@gmail.com>
2018-03-05 21:15:31 +0900

    Update: - Added Loading Screen - Added Volume control in Options Menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,16 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuScript : MonoBehaviour {
 
-	public void PlayGame()
+	public GameObject loadingScreen;
+	public Slider loadingSlider;
+	public TextMeshProUGUI progressText;
+
+	public void PlayGame(string name)
 	{
-		SceneManager.LoadScene("Main");
+		StartCoroutine(LoadingAsynchronously(name));
 	}
 
 	public void QuitGame()
 	{
 		Debug.Log("Quit!");
 		Application.Quit();
+	}
+
+	IEnumerator LoadingAsynchronously (string name)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+		loadingScreen.SetActive(true);
+
+		while(!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+			loadingSlider.value = progress;
+			progressText.text = progress * 100  + "%";
+
+			yield return null;
+		}
 	}
 }
