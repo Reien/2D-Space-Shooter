afc979ca8533108122e7d831d9373cdc88f64eda
VICTORIAMK3\Jude <reien1012@gmail.com>
2018-03-11 15:15:47 +0900

    Update: - Added Sounds from free asset packs (credits to owner) - Updated Enemy Assets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject playButton;
-	public GameObject playerShip;
-
-	public enum GameManagerState
-	{
-		Opening,
-		Gameplay,
-		GameOver,
-	}
-
-	GameManagerState GMState;
-
 	// Use this for initialization
 	void Start () {
-		GMState = GameManagerState.Opening;
+		
 	}
 	
 	// Update is called once per frame
